161818b7945d2dbe4226d769b692049d8dc170b3
zhivar <zhivarsourati@gmail.com>
2020-04-06 01:45:20 +0430

    Add authentication

--- multi_thread_client.py
+++ multi_thread_client.py
@@ -1,54 +1,5 @@
 # Import socket module
 import socket
-import json
-
-
-bad_sequence_of_commands = '503 Bad sequence of commands.'
-invalid_username_or_pass = '403 Invalid username or password.'
-username_ok = '331 User name okey, need password.'
-login_ok = '230 User logged in, proceed.'
-
-
-config_data = None
-logged_in = False
-
-
-def read_config_file():
-    global config_data
-    config_file = open('config.json')
-    config_data = json.load(config_file)
-
-
-def get_users():
-    return config_data['users']
-
-
-def find_user(user_name):
-    for user in get_users():
-        if user['user'] == user_name:
-            return user
-    return None
-
-
-def login():
-    username_inputs = list(map(str, input().split()))
-    if (username_inputs[0] != 'USER'):
-        print(bad_sequence_of_commands)
-        return 0
-    user_name = username_inputs[1][1:len(username_inputs[1]) - 1]
-    user = find_user(user_name)
-    if not user:
-        print(invalid_username_or_pass)
-        return 0
-    password_inputs = list(map(str, input().split()))
-    if (password_inputs[0] != 'PASS'):
-        print(bad_sequence_of_commands)
-        return 0
-    password = password_inputs[1][1:len(password_inputs[1]) - 1]
-    if password != user['password']:
-        print(invalid_username_or_pass)
-        return 0
-    print(login_ok)
 
 
 def Main():
@@ -66,16 +17,13 @@ def Main():
     # message you send to server
     print("starting mail server")
 
-    message = "zhivar mohsen"
-
     while True:
 
-        if (not logged_in):
-            print('pls login')
-            login()
+        data = input()
 
         # message sent to server
-        s.send(message.encode('ascii'))
+
+        s.send(data.encode())
 
         # messaga received from server
         data = s.recv(1024)
@@ -96,5 +44,4 @@ def Main():
 
 
 if __name__ == '__main__':
-    read_config_file()
     Main()
--- multi_thread_server.py
+++ multi_thread_server.py
@@ -1,32 +1,99 @@
 # import socket programming library
 import socket
+import json
 
 # import thread module
 from _thread import *
 import threading
+
+
+config_data = None
+
+BAD_SEQUENCE_OF_COMMANDS = '503 Bad sequence of commands.'
+INVALID_USERNAME_PASSWORD = '403 Invalid username or password.'
+USERNAME_OK = '331 User name okey, need password.'
+LOGIN_OK = '230 User logged in, proceed.'
+NOT_AUTHORIZED = '332 Need account for login.'
+MAX_SIZE = 1024
+SYNTAX_ERROR = '501 Syntax error in parameters or arguments.'
+
 
 # print_lock = threading.Lock()
 
 # thread function
 
 
+def read_config_file():
+    global config_data
+    config_file = open('config.json')
+    config_data = json.load(config_file)
+
+
+def get_users():
+    return config_data['users']
+
+
+def find_user(user_name):
+    for user in get_users():
+        if user['user'] == user_name:
+            return user
+    return None
+
+
 def threaded(c):
+    logged_in = False
+    user_username = None
+    user = None
     while True:
 
         # data received from client
-        data = c.recv(1024)
+        data = (c.recv(1024)).decode()
+
         if not data:
             print('Bye')
-
             # lock released on exit
             # print_lock.release()
             break
 
-        # reverse the given string from client
-        data = data[::-1]
+        parsed_data = list(map(str, data.split()))
+        if (not logged_in) and (parsed_data[0] not in ['USER', 'PASS']):
+            c.send(NOT_AUTHORIZED.encode())
+            continue
+        if (not logged_in) and (parsed_data[0] in ['USER', 'PASS']):
+            if not user_username:
+                if parsed_data[0] != 'USER':
+                    c.send(BAD_SEQUENCE_OF_COMMANDS.encode())
+                    continue
+                if parsed_data[1][0] != '<' or parsed_data[1][len(parsed_data[1]) - 1] != '>':
+                    c.send(SYNTAX_ERROR.encode())
+                input_username = parsed_data[1][1:len(parsed_data[1]) - 1]
+                user = find_user(input_username)
+                if not user:
+                    c.send(INVALID_USERNAME_PASSWORD.encode())
+                    continue
+                user_username = user['user']
+                c.send(USERNAME_OK.encode())
+                continue
+            else:
+                if parsed_data[0] != 'PASS':
+                    c.send(BAD_SEQUENCE_OF_COMMANDS.encode())
+                    continue
+                if parsed_data[1][0] != '<' or parsed_data[1][len(parsed_data[1]) - 1] != '>':
+                    c.send(SYNTAX_ERROR.encode())
+                input_password = parsed_data[1][1:len(parsed_data[1]) - 1]
+                if input_password != user['password']:
+                    c.send(INVALID_USERNAME_PASSWORD.encode())
+                    continue
+                c.send(LOGIN_OK.encode())
+                logged_in = True
+                continue
+        if(logged_in):
+            c.send('user is logged in'.encode())
+            continue
 
-        # send back reversed string to client
-        c.send(data)
+            # reverse the given string from client
+
+            # send back reversed string to client
 
     # connection closed
     c.close()
@@ -62,4 +129,5 @@ def Main():
 
 
 if __name__ == '__main__':
+    read_config_file()
     Main()
